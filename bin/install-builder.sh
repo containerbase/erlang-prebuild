ca4ceadda4747a1f3f1340ad7b559c87d54bfa88
Michael Kriese <michael.kriese@visualon.de>
2022-03-04 11:40:35 +0100

    fix: add missing packages

--- bin/install-builder.sh
+++ bin/install-builder.sh
@@ -14,41 +14,20 @@ fi
 
 export DEBIAN_FRONTEND=noninteractive
 
-# https://github.com/php-build/php-build/blob/6530e7501ccc758928d5510813dc3f5fbdc87419/install-dependencies.sh#L34
 apt-get update -q
-# apt-get install -q -y \
-#   autoconf2.13 \
-#   autoconf2.64 \
-#   autoconf \
-#   bison \
-#   build-essential \
-#   ca-certificates \
-#   curl \
-#   dumb-init \
-#   git \
-#   file \
-#   libbz2-dev \
-#   libc-client2007e-dev \
-#   libcurl4-openssl-dev \
-#   libicu-dev \
-#   libjpeg-dev \
-#   libkrb5-dev \
-#   libmcrypt-dev \
-#   libonig-dev \
-#   libpng-dev \
-#   libreadline-dev \
-#   libsqlite3-dev \
-#   libssl-dev \
-#   libtidy-dev \
-#   libxml2-dev \
-#   libxslt-dev \
-#   libzip-dev \
-#   pkg-config \
-#   re2c \
-#   zlib1g-dev \
-#   ;
+apt-get install -q -y \
+  autoconf \
+  automake \
+  build-essential \
+  ca-certificates \
+  curl \
+  dumb-init \
+  git \
+  libncurses5-dev \
+  libssl-dev \
+  ;
 
-curl -o /usr/local/bin/kerl https://raw.githubusercontent.com/kerl/kerl/master/kerl
+curl -sSLfo /usr/local/bin/kerl https://raw.githubusercontent.com/kerl/kerl/master/kerl
 chmod +x /usr/local/bin/kerl
 mkdir -p /usr/local/erlang /cache
 
